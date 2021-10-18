d513e1e7148de5ffa883da4ee84aef493937f881
dkozina <dkozina@srv.rbt>
2021-10-18 21:43:41 +0300

    horizonte, essen: basic translation with tooltips

---
+++ src/components/Tooltip.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class='tooltip'>{{text}}</div>
+</template>
+
+<script>
+export default {
+  name: 'Tooltip',
+  props: {
+    text: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
--- src/views/Essen.vue
+++ src/views/Essen.vue
@@ -1,42 +1,66 @@
 <template>
-  <div class='page essen'>
-    <router-link class='go-back' :to="{name: 'horizonte-6'}">Zurück</router-link>
-    <h1 class='primary-title centered'>Das Essen Еда</h1>
-        <table class='table'>
-            <tr>
-                <th>Das esse ich gerne</th>
-                <th>Das esse ich nicht so gerne</th>
-                <th>Das mag ich überhaupt nicht</th>
-            </tr>
-            <tr
-              v-for='(item, i) in eatingTableRows'
-              :key='i'
-            >
-              <td class='gerne'>
-                <input
-                  class='eating-input'
-                  v-model="item.gerne"
-                  type="text"
-                  @change='saveInfo'></td>
-              <td class='nicht-gerne'>
-                <input
-                  class='eating-input'
-                  v-model="item.nichtGerne"
-                  type="text"
-                  @change='saveInfo'></td>
-              <td class='nicht'>
-                <input
-                  class='eating-input'
-                  v-model="item.nicht"
-                  type="text"
-                  @change='saveInfo'></td>
-            </tr>
-        </table>
-        <button
-          class='btn add-row'
-          @click='addRow'
-        >+
-        </button>
+  <div class='page lesson essen'>
+    <header>
+      <router-link class='go-back' :to="{name: 'horizonte-6'}">Zurück</router-link>
+      <button class='btn show-tooltips'  @click='toggleTooltips'>Übersetzen</button>
+    </header>
+    <h1 class='primary-title centered has-tooltip'>
+      Das Essen
+      <Tooltip
+        v-if="tooltipShown"
+        text="Еда"
+      />
+    </h1>
+    <table class='table'>
+        <tr>
+            <th>Das esse ich
+              <span class='has-tooltip'>gerne
+                <Tooltip
+                  v-if="tooltipShown"
+                  text="Охотно"
+                />
+              </span>
+            </th>
+            <th>Das esse ich nicht so gerne</th>
+            <th>Das mag ich
+              <span class='has-tooltip'>
+                überhaupt nicht
+                <Tooltip
+                  v-if="tooltipShown"
+                  text="Вообще нет"
+                />
+              </span>
+            </th>
+        </tr>
+        <tr
+          v-for='(item, i) in eatingTableRows'
+          :key='i'
+        >
+          <td class='gerne'>
+            <input
+              class='eating-input'
+              v-model="item.gerne"
+              type="text"
+              @change='saveInfo'></td>
+          <td class='nicht-gerne'>
+            <input
+              class='eating-input'
+              v-model="item.nichtGerne"
+              type="text"
+              @change='saveInfo'></td>
+          <td class='nicht'>
+            <input
+              class='eating-input'
+              v-model="item.nicht"
+              type="text"
+              @change='saveInfo'></td>
+        </tr>
+    </table>
+    <button
+      class='btn add-row'
+      @click='addRow'
+    >+
+    </button>
 
         <div class='present'>
             <h2>Presens Настоящее время</h2>
@@ -123,11 +147,17 @@
   </div>
 </template>
 <script>
+import Tooltip from '@/components/Tooltip.vue';
+
 export default {
   name: 'Essen',
+  components: {
+    Tooltip,
+  },
   data() {
     return {
       eatingTableRows: [],
+      tooltipShown: false,
     };
   },
   created() {
@@ -147,6 +177,13 @@ export default {
     },
     saveInfo() {
       localStorage.setItem('eating-table', JSON.stringify(this.eatingTableRows));
+    },
+    toggleTooltips() {
+      if (this.tooltipShown) {
+        this.tooltipShown = false;
+      } else {
+        this.tooltipShown = true;
+      }
     },
   },
 };
