4069b8108d40d3adbf5dabc230b5ec8d3ece3a52
Daria Kozina <kozina.flowers@yandex.ru>
2021-10-30 19:10:51 +0300

    ege: basic menu added

---
+++ src/views/EGE.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="page ege">
+    <header>
+      <router-link class='go-back light' :to="{name: 'home'}">Go back</router-link>
+    </header>
+    <div class='contents'>
+      <h1 class='primary-title heading-light'>Contents</h1>
+      <ul>
+          <li>
+            <router-link class='light' :to="{name: 'ege-dictionaries'}">Dictionaries</router-link>
+          </li>
+        <!-- <li><router-link :to="{name: 'en-word-forms'}">Word forms</router-link></li> -->
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EGE',
+};
+</script>
