cd76628306939a9ee64630427b0c60fdff40b515
dkozina <dkozina@srv.rbt>
2022-01-17 13:48:49 +0300

    ege: dictionaries page refactored

---
+++ src/components/ContentsList.vue
@@ -0,0 +1,42 @@
+<template>
+    <ol class='contents-list'>
+      <li
+        v-for="line in contentsList"
+        :key="line.moveTo"
+        class='contents-list__line'
+      >
+        <a :href='`#${line.moveTo}`' class='link'>{{ line.title }}</a>
+        <ol v-if="line.sublist">
+          <li
+            v-for="subline in line.sublist"
+            :key="subline.moveTo"
+            class='contents-list__line'
+          >
+            <a :href='`#${subline.moveTo}`'>
+              <span class='contents-list__unique-number'>{{ subline.uniqueNumber + " " }}</span>
+              <span class='contents-list__topic'>{{ subline.title }}</span>
+            </a>
+          </li>
+          <!--
+          <li class='contents-list__line'>
+            <a href='#n1-176'>
+              <span class='contents-list__unique-number'>№1: 176 </span>
+              <span class='contents-list__topic'>Listening about work</span>
+            </a>
+          </li> -->
+        </ol>
+      </li>
+    </ol>
+</template>
+
+<script>
+export default {
+  name: 'ContentsList',
+  props: {
+    contentsList: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
---
+++ src/components/lessons/LinkingWords.vue
@@ -0,0 +1,142 @@
+<template>
+  <h2 class='secondary-title heading-light'>Linking words</h2>
+  <div class='linking-words'>
+    <div class='dictionary__item-inner'>
+      <ul>
+        <li
+          class='linking-words__line'
+          v-for='item in linking_words.en'
+          :key='item'
+        >
+        {{item}}
+        </li>
+      </ul>
+      <ul>
+        <li
+          class='linking-words__line-ru'
+          v-for='item in linking_words.ru'
+          :key='item'
+        >
+        {{item}}
+        </li>
+      </ul>
+    </div>
+    <ol>
+      <li class='linking-words__example'>
+        On receipt of a call the 999 Room operator ascertains by
+        electronic device the position of the nearest available police car
+        <InputAnswer
+          word=''
+          ending=''
+          readyFullWord='which'
+        />
+        is contacted by radio
+      </li>
+      <li class='linking-words__example'>
+        Make sure
+        <InputAnswer
+          word=''
+          ending=''
+          readyFullWord='that'
+        />
+        this policeman will bring the criminal to justice
+      </li>
+      <li class='linking-words__example'>
+        One dog, for example, can search a warehouse in ten minutes,
+        <InputAnswer
+          word=''
+          ending=''
+          readyFullWord='whereas'
+        />
+        the same search would take six men an hour
+      </li>
+      <li class='linking-words__example'>
+        These horses are particularly suited to ceremonial occasions,
+        <InputAnswer
+          word=''
+          ending=''
+          readyFullWord='for'
+        />
+        they are accustomed to military bands
+      </li>
+      <li class='linking-words__example'>
+        Retail is still a big employer,
+        <InputAnswer
+          word=''
+          ending=''
+          readyFullWord='but'
+        />
+        a lot more of it is being done online
+      </li>
+      <li class='linking-words__example'>
+        I could not understand
+        <InputAnswer
+          word=''
+          ending=''
+          readyFullWord='whether'
+        />
+        she improved her English skills or not
+      </li>
+      <li class='linking-words__example'>
+        It seemed
+        <InputAnswer
+          word=''
+          ending=''
+          readyFullWord='like'
+        />
+        it's going to rain soon
+      </li>
+      <li class='linking-words__example'>
+        This research,
+        <InputAnswer
+          word=''
+          ending=''
+          readyFullWord='as well as'
+        />
+        my own thoughts made me think that the Egyptian pyramids
+        were not built by slaves without any modern technologies.
+      </li>
+    </ol>
+  </div>
+</template>
+
+<script>
+import InputAnswer from '@/components/InputAnswer.vue';
+
+export default {
+  name: 'LinkingWords',
+  components: { InputAnswer },
+  data() {
+    return {
+      linking_words: {
+        en: [
+          'which',
+          'as well as',
+          'that',
+          'whether',
+          'so',
+          'because',
+          'for',
+          'but',
+          'like',
+          'such as',
+          'whereas',
+        ],
+        ru: [
+          'который',
+          'так же, как и',
+          'что',
+          'ли',
+          'так, таким образом',
+          'потому что',
+          'так как',
+          'но',
+          'как (сравнение)',
+          'такие (-ой, -ая) как',
+          'в то время как',
+        ],
+      },
+    };
+  },
+};
+</script>
