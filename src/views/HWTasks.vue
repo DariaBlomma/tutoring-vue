9ba2f8be6a6619431494c07db0dbd2786e6d0502
Дарья Козина <kozina.flowers@yandex.ru>
2022-02-23 21:28:05 +0300

    ege: lesson plans and hw tasks updated

--- src/views/HWTasks.vue
+++ src/views/HWTasks.vue
@@ -17,163 +17,178 @@
   </div>
 </template>
 
-<script>
+<script setup>
 import ColorStates from '@/components/ColorStates.vue';
 import ScrollTopBtn from '@/components/ScrollTopBtn.vue';
 import PlanTable from '@/components/PlanTable.vue';
 
-export default {
-  name: 'HWTasks',
-  components: {
-    ColorStates,
-    ScrollTopBtn,
-    PlanTable,
+const hwColorsList = [
+  {
+    class: 'actual',
+    explanation: 'Актуальное дз',
   },
-  data() {
-    return {
-      hwColorsList: [
+  {
+    class: 'done',
+    explanation: 'Выполненный пункт',
+  },
+  {
+    class: 'partially-done',
+    explanation: 'Частично выполненный пункт',
+  },
+  {
+    class: 'debt',
+    explanation: 'Долг',
+  },
+  {
+    class: 'order-important',
+    explanation: 'Важен порядок выполнения пунктов',
+  },
+  {
+    class: 'lesson-required',
+    explanation: 'Нужно для урока',
+  },
+];
+const hws = [
+  [
+    {},
+    {},
+    {},
+    {
+      actual: false,
+      date: '26.12.21',
+      list: [
+        ['Перевести устно прикрепленное в телеграме аудио (максимально близко к оригиналу, опираясь на ранее переведенные слова в словариках)', 'done'],
+        ['Смотреть видео Continuous tenses и исправить свое образование Continuous в Present, Past, Future (например, с глаголом come )', 'done'],
+        ['Слушать песню On the railroad. Проверить отстутствие технических проблем и параллельно смотреть слова (есть справа на сайте Яндекс Музыки)', 'done'],
+      ],
+      links: [
         {
-          class: 'actual',
-          explanation: 'Актуальное дз',
+          address: 'https://youtu.be/k-9o1RK1AkQ',
+          title: 'Continuous tenses',
         },
         {
-          class: 'done',
-          explanation: 'Выполненный пункт',
-        },
-        {
-          class: 'partially-done',
-          explanation: 'Частично выполненный пункт',
-        },
-        {
-          class: 'debt',
-          explanation: 'Долг',
-        },
-        {
-          class: 'order-important',
-          explanation: 'Важен порядок выполнения пунктов',
-        },
-        {
-          class: 'lesson-required',
-          explanation: 'Нужно для урока',
+          address: 'https://music.yandex.ru/album/5421271/track/41395019',
+          title: 'On the railroad',
         },
       ],
-      hws: [
-        [
-          {},
-          {},
-          {},
-          {
-            actual: false,
-            date: '26.12.21',
-            list: [
-              ['Перевести устно прикрепленное в телеграме аудио (максимально близко к оригиналу, опираясь на ранее переведенные слова в словариках)', 'done'],
-              ['Смотреть видео Continuous tenses и исправить свое образование Continuous в Present, Past, Future (например, с глаголом come )', 'done'],
-              ['Слушать песню On the railroad. Проверить отстутствие технических проблем и параллельно смотреть слова (есть справа на сайте Яндекс Музыки)', 'done'],
-            ],
-            links: [
-              {
-                address: 'https://youtu.be/k-9o1RK1AkQ',
-                title: 'Continuous tenses',
-              },
-              {
-                address: 'https://music.yandex.ru/album/5421271/track/41395019',
-                title: 'On the railroad',
-              },
-            ],
-          },
-        ],
-        [
-          {
-            actual: false,
-            addInfo: `
-              <h4 class='title-4'>Как работать с аудированием</h4>
-              <ol class='add-info__list'>
-                <li class='add-info__line'>прослушать часть предложения</li>
-                <li class='add-info__line'>сказать ее по английски</li>
-                <li class='add-info__line'>перевести ее на русский</li>
-                <li class='add-info__line'>анализировать (каждый пункт проговорить вслух)
-                  <ul class='list-typed'>
-                    <li class='add-info__line'>Если в русском переводе больше слов, чем в английском, то какого слова не хватает в моем английском переводе?</li>
-                    <li class='add-info__line'>Если я не слышу какого-то слова, то надо разобрать по частям речи имеющееся и понять, какой части речи не хватает</li>
-                    <li class='add-info__line'>В каком времени сказано предложение? Как в этом времени тогда будут образованы глаголы в предложении? Слышу ли я это? Сказали ли я отчетливо это в своей речи?</li>
-                  </ul>
-                </li>
-                <li class='add-info__line'>после анализа сказать еще раз (уже в исправленном виде) предложение по английски и перевести на русский</li>
-              </ol>
-            `,
-            date: '28.12.21',
-            list: [
-              ['Записать на английском речь Speaker A из Задания 176 на аудирование', 'done'],
-              ['Разобрать речь Speaker B, С согласно плану Как работать с аудированием. Каждый пункт проговаривать вслух в качестве отчета', 'debt'],
-              ['Потренироваться говорить в том же темпе, как и оригинал, песню On the railroad. Прислать аудиозапись получившегося', 'done'],
-              [`Найти в интернете английские песни на рождество или новый год,
-                те, которые ты где-либо слышала и те, которые тебе просто понравились. Несколько штук.
-                Может помочь в поиске словосочетание Christmas carols, new year songs
-                Прислать ссылки, где можно послушать эти песни`, 'done'],
-            ],
-            links: [
-              {
-                address: 'https://en-ege.sdamgia.ru/problem?id=176',
-                title: 'Задание ЕГЭ 176 на аудирование (аудио запись)',
-              },
-              {
-                address: 'https://music.yandex.ru/album/5421271/track/41395019',
-                title: 'On the railroad',
-              },
-            ],
-          },
-          {},
-          {
-            actual: false,
-            date: '15.01.22',
-            list: [
-              ['Пункты 2 и 3 из дз на 28.12.21', 'partially-done'],
-              ['Прослушать песню в Телеграме My heart will go on', 'done'],
-            ],
-            links: [
-              {
-                address: 'https://youtu.be/gGraiFN283E',
-                title: '(Guitar cover) My heart will go on',
-              },
-            ],
-          },
-          {},
-        ],
-        [
-          {
-            actual: true,
-            date: '18.01.22',
-            list: [
-              ['Пункт 2 дз из на 28.12.21', 'lesson-required'],
-              ['Заполнить таблицу словообразования на основе словарика Feelings about music',
-                'order-important'],
-              ['Прослушать оригинальную песню Celine Dion My heart will go on',
-                'order-important'],
-              [`Сравнить свои ощущения от гитарной и оригинальной версии этой песни,
-                использую слова из указанного выше словарикаб НО! в другой части речи
-                (взять из заполненной в пункте 2 таблицы)`,
-              // eslint-disable-next-line indent
-                'order-important'],
-            ],
-            links: [
-              {
-                address: 'https://miro.com/app/board/o9J_kuE-FSk=/?moveToWidget=3458764516690915363&cot=14',
-                title: 'Таблица словообразования на миро',
-              },
-              {
-                address: 'https://daria-blomma-tutoring.herokuapp.com/dictionaries#music_feelings',
-                title: 'Cловарик Feelings about music',
-              },
-              {
-                address: 'https://youtu.be/8u_T0ZEVD24',
-                title: 'Celine Dion My heart will go on',
-              },
-            ],
-          },
-          {}, {}, {},
-        ],
+    },
+  ],
+  [
+    {
+      actual: false,
+      addInfo: `
+        <h4 class='title-4'>Как работать с аудированием</h4>
+        <ol class='add-info__list'>
+          <li class='add-info__line'>прослушать часть предложения</li>
+          <li class='add-info__line'>сказать ее по английски</li>
+          <li class='add-info__line'>перевести ее на русский</li>
+          <li class='add-info__line'>анализировать (каждый пункт проговорить вслух)
+            <ul class='list-typed'>
+              <li class='add-info__line'>Если в русском переводе больше слов, чем в английском, то какого слова не хватает в моем английском переводе?</li>
+              <li class='add-info__line'>Если я не слышу какого-то слова, то надо разобрать по частям речи имеющееся и понять, какой части речи не хватает</li>
+              <li class='add-info__line'>В каком времени сказано предложение? Как в этом времени тогда будут образованы глаголы в предложении? Слышу ли я это? Сказали ли я отчетливо это в своей речи?</li>
+            </ul>
+          </li>
+          <li class='add-info__line'>после анализа сказать еще раз (уже в исправленном виде) предложение по английски и перевести на русский</li>
+        </ol>
+      `,
+      date: '28.12.21',
+      list: [
+        ['Записать на английском речь Speaker A из Задания 176 на аудирование', 'done'],
+        ['Разобрать речь Speaker B, С согласно плану Как работать с аудированием. Каждый пункт проговаривать вслух в качестве отчета', 'debt'],
+        ['Потренироваться говорить в том же темпе, как и оригинал, песню On the railroad. Прислать аудиозапись получившегося', 'done'],
+        [`Найти в интернете английские песни на рождество или новый год,
+          те, которые ты где-либо слышала и те, которые тебе просто понравились. Несколько штук.
+          Может помочь в поиске словосочетание Christmas carols, new year songs
+          Прислать ссылки, где можно послушать эти песни`, 'done'],
       ],
-    };
-  },
-};
+      links: [
+        {
+          address: 'https://en-ege.sdamgia.ru/problem?id=176',
+          title: 'Задание ЕГЭ 176 на аудирование (аудио запись)',
+        },
+        {
+          address: 'https://music.yandex.ru/album/5421271/track/41395019',
+          title: 'On the railroad',
+        },
+      ],
+    },
+    {},
+    {
+      actual: false,
+      date: '15.01.22',
+      list: [
+        ['Пункты 2 и 3 из дз на 28.12.21', 'partially-done'],
+        ['Прослушать песню в Телеграме My heart will go on', 'done'],
+      ],
+      links: [
+        {
+          address: 'https://youtu.be/gGraiFN283E',
+          title: '(Guitar cover) My heart will go on',
+        },
+      ],
+    },
+    {},
+  ],
+  [
+    {
+      actual: false,
+      date: '18.01.22',
+      list: [
+        ['Пункт 2 дз из на 28.12.21', 'lesson-required'],
+        ['Заполнить таблицу словообразования на основе словарика Feelings about music',
+          'order-important'],
+        ['Прослушать оригинальную песню Celine Dion My heart will go on',
+          'order-important'],
+        [`Сравнить свои ощущения от гитарной и оригинальной версии этой песни,
+          использую слова из указанного выше словарикаб НО! в другой части речи
+          (взять из заполненной в пункте 2 таблицы)`,
+        // eslint-disable-next-line indent
+          'order-important'],
+      ],
+      links: [
+        {
+          address: 'https://miro.com/app/board/o9J_kuE-FSk=/?moveToWidget=3458764516690915363&cot=14',
+          title: 'Таблица словообразования на миро',
+        },
+        {
+          address: 'https://daria-blomma-tutoring.herokuapp.com/dictionaries#music_feelings',
+          title: 'Cловарик Feelings about music',
+        },
+        {
+          address: 'https://youtu.be/8u_T0ZEVD24',
+          title: 'Celine Dion My heart will go on',
+        },
+      ],
+    },
+    {},
+    {
+      actual: true,
+      date: '26.02.22',
+      list: [
+        ['Рассказать кратко (5 - 10 предложений) о текущей политической ситуации.'],
+        ['Статьи по теме на английском можно найти, если искать в Google - russia recognized independence'],
+        ['В каждом предложении использоваать сложные слова.'],
+        ['Может помочь мини-словарик Some politics of 2022 (ссылка приложена внизу)'],
+      ],
+      addInfo: `
+        <h4 class='title-4'>Примерный план рассказа</h4>
+        <ol class='add-info__list'>
+          <li class='add-info__line'>Когда и почему ухудшились отношения между Украиной и нынешними ЛНР/ДНР (между восточной и западной частями страны)</li>
+          <li class='add-info__line'>Реакция России на этот конфликт</li>
+          <li class='add-info__line'>Отношение Запада к сложившейся ситуации</li>
+          <li class='add-info__line'>Как произошло признание ДНР/ЛНР - кто был инициатором, кто как относился к возможному признанию?</li>
+          <li class='add-info__line'>Что меняет факт признания - для самих республик Украины, для Росии, для Запада и США?</li>
+        </ol>
+      `,
+      links: [
+        {
+          title: 'Some politics of 2022',
+          address: 'https://daria-blomma-tutoring.herokuapp.com/lesson-materials#politics_2022',
+        },
+      ],
+    },
+    {},
+  ],
+];
 </script>
--- src/views/LessonPlans.vue
+++ src/views/LessonPlans.vue
@@ -19,228 +19,257 @@
   </div>
 </template>
 
-<script>
+<script setup>
 // todo 1) скролл к актуальному плану
+// todo 2) адаптация под мобильный
 import ColorStates from '@/components/ColorStates.vue';
 import ScrollTopBtn from '@/components/ScrollTopBtn.vue';
 import PlanTable from '@/components/PlanTable.vue';
 
-export default {
-  name: 'LessonPlans',
-  components: {
-    ColorStates,
-    ScrollTopBtn,
-    PlanTable,
+const plansColorsList = [
+  {
+    class: 'actual',
+    explanation: 'Актуальный план',
   },
-  setup() {
-    const plansColorsList = [
-      {
-        class: 'actual',
-        explanation: 'Актуальный план',
-      },
-      {
-        class: 'done',
-        explanation: 'Выполненный пункт',
-      },
-      {
-        class: 'partially-done',
-        explanation: 'Частично выполненный пункт',
-      },
-      {
-        class: 'debt',
-        explanation: 'Долг',
-      },
-      {
-        class: 'order-important',
-        explanation: 'Важен порядок выполнения пунктов',
-      },
-    ];
-    const lessonPlans = [
-      [
-        {},
-        {
-          actual: false,
-          date: '27.01.22',
-          time: '1 час',
-          list: [
-            [`Сравнить ощущения от гитарной и оригинальной версии песни My heart will go on.
+  {
+    class: 'done',
+    explanation: 'Выполненный пункт',
+  },
+  {
+    class: 'partially-done',
+    explanation: 'Частично выполненный пункт',
+  },
+  {
+    class: 'debt',
+    explanation: 'Долг',
+  },
+  {
+    class: 'order-important',
+    explanation: 'Важен порядок выполнения пунктов',
+  },
+];
+const lessonPlans = [
+  [
+    {},
+    {
+      actual: false,
+      date: '27.01.22',
+      time: '1 час',
+      list: [
+        [`Сравнить ощущения от гитарной и оригинальной версии песни My heart will go on.
                 Использовать сделанную таблицу словообразования на миро -
                 использовать другие части речи, не те, что были даны в словарике и выделены в таблице желтым`, 'done'],
-            ['Разобрать речь Speaker B до конца согласно плану Как работать с аудированием.', 'done'],
-          ],
-          links: [
-            {
-              address: 'https://miro.com/app/board/o9J_kuE-FSk=/?moveToWidget=3458764516690915363&cot=14',
-              title: 'Таблица словообразования на миро',
-            },
-            {
-              address: 'https://daria-blomma-tutoring.herokuapp.com/dictionaries#music_feelings',
-              title: 'Cловарик Feelings about music',
-            },
-            {
-              address: 'https://youtu.be/8u_T0ZEVD24',
-              title: 'Celine Dion My heart will go on',
-            },
-            {
-              address: 'https://youtu.be/gGraiFN283E',
-              title: '(Guitar cover) My heart will go on',
-            },
-            {
-              address: 'https://en-ege.sdamgia.ru/problem?id=176',
-              title: 'Задание ЕГЭ 176 на аудирование (аудио запись)',
-            },
-          ],
+        ['Разобрать речь Speaker B до конца согласно плану Как работать с аудированием.', 'done'],
+      ],
+      links: [
+        {
+          address: 'https://miro.com/app/board/o9J_kuE-FSk=/?moveToWidget=3458764516690915363&cot=14',
+          title: 'Таблица словообразования на миро',
         },
         {
-          actual: false,
-          date: '29.01.22',
-          time: '1 час',
-          list: [
-            ['Записать на английском речь Speaker B из Задания 176 на аудирование', 'done'],
-            ['Обсудить речь Speaker B - проблемы, трудности, преимущества этой профессии.', 'done'],
-          ],
+          address: 'https://daria-blomma-tutoring.herokuapp.com/dictionaries#music_feelings',
+          title: 'Cловарик Feelings about music',
         },
         {
-          actual: false,
-          date: '30.01.22',
-          time: '30 мин',
-          list: [
-            ['Фонетическая разминка - песня Everybody walking this land', 'done'],
-          ],
-          links: [
-            {
-              address: 'https://youtu.be/q5H1i2GBxEc',
-              title: 'Home Free - Everybody walking this land',
-            },
-            {
-              address: 'https://www.azlyrics.com/lyrics/homefree/everybodywalkinthisland.html',
-              title: 'Lyrics for Everybody walking this land',
-            },
-          ],
+          address: 'https://youtu.be/8u_T0ZEVD24',
+          title: 'Celine Dion My heart will go on',
+        },
+        {
+          address: 'https://youtu.be/gGraiFN283E',
+          title: '(Guitar cover) My heart will go on',
+        },
+        {
+          address: 'https://en-ege.sdamgia.ru/problem?id=176',
+          title: 'Задание ЕГЭ 176 на аудирование (аудио запись)',
         },
       ],
-      [
+    },
+    {
+      actual: false,
+      date: '29.01.22',
+      time: '1 час',
+      list: [
+        ['Записать на английском речь Speaker B из Задания 176 на аудирование', 'done'],
+        ['Обсудить речь Speaker B - проблемы, трудности, преимущества этой профессии.', 'done'],
+      ],
+    },
+    {
+      actual: false,
+      date: '30.01.22',
+      time: '30 мин',
+      list: [
+        ['Фонетическая разминка - песня Everybody walking this land', 'done'],
+      ],
+      links: [
         {
-          actual: false,
-          date: '01.02.2022',
-          time: '30 мин',
-          list: [
-            ['Разобрать речь Speaker C  согласно плану Как работать с аудированием.', 'done'],
-          ],
+          address: 'https://youtu.be/q5H1i2GBxEc',
+          title: 'Home Free - Everybody walking this land',
         },
         {
-          actual: false,
-          date: '03.02.2022',
-          time: '30 мин',
-          list: [
-            ['Обсудить речь Speaker C - проблемы, трудности, преимущества этой профессии.'],
-            ['Записать на английском речь Speaker C из Задания 176 на аудирование'],
-          ],
-        },
-        {
-          actual: false,
-          date: '05.02.2022',
-          time: '30 мин',
-          list: [
-            ['Проработать песню Everybody walking this land, строки с быстрым ритмом и речь певца в конце подробно', 'done'],
-          ],
-        },
-        {
-          actual: false,
-          date: '06.02.2022',
-          time: '60 мин',
-          list: [
-            ['Разговор на общие темы - как прошла неделя и т.д', 'done'],
-            ['Обсудить речь Speaker C - проблемы, трудности, преимущества этой профессии.', 'done'],
-            ['Записать на английском речь Speaker C из Задания 176 на аудирование'],
-            ['Разобрать речь Speaker D'],
-          ],
+          address: 'https://www.azlyrics.com/lyrics/homefree/everybodywalkinthisland.html',
+          title: 'Lyrics for Everybody walking this land',
         },
       ],
-      [
+    },
+  ],
+  [
+    {
+      actual: false,
+      date: '01.02.2022',
+      time: '30 мин',
+      list: [
+        ['Разобрать речь Speaker C  согласно плану Как работать с аудированием.', 'done'],
+      ],
+    },
+    {
+      actual: false,
+      date: '03.02.2022',
+      time: '30 мин',
+      list: [
+        ['Обсудить речь Speaker C - проблемы, трудности, преимущества этой профессии.'],
+        ['Записать на английском речь Speaker C из Задания 176 на аудирование'],
+      ],
+    },
+    {
+      actual: false,
+      date: '05.02.2022',
+      time: '30 мин',
+      list: [
+        ['Проработать песню Everybody walking this land, строки с быстрым ритмом и речь певца в конце подробно', 'done'],
+      ],
+    },
+    {
+      actual: false,
+      date: '06.02.2022',
+      time: '60 мин',
+      list: [
+        ['Разговор на общие темы - как прошла неделя и т.д', 'done'],
+        ['Обсудить речь Speaker C - проблемы, трудности, преимущества этой профессии.', 'done'],
+        ['Записать на английском речь Speaker C из Задания 176 на аудирование'],
+        ['Разобрать речь Speaker D'],
+      ],
+    },
+  ],
+  [
+    {
+      actual: false,
+      date: '08.02.2022',
+      time: '30 мин',
+      list: [
+        ['Записать на английском речь Speaker C из Задания 176 на аудирование', 'done'],
+        ['Разобрать речь Speaker D'],
+      ],
+    },
+    {},
+    {
+      actual: false,
+      date: '12.02.2022',
+      time: '60 мин',
+      list: [
+        ['Разобрать речь Speaker D', 'done'],
+      ],
+    },
+    {
+      actual: false,
+      date: '13.02.2022',
+      time: '30 мин',
+      list: [
+        ['Разговор на общие темы - про пищевые привычки в семье', 'done'],
+      ],
+    },
+  ],
+  // 14.02 - 20.02
+  [
+    {
+    },
+    {
+      actual: false,
+      date: '17.02.2022',
+      time: '30 мин',
+      list: [
+        ['Записать на английском речь Speaker D из Задания 176 на аудирование', 'done'],
+        ['Обсудить речь Speaker D - проблемы, трудности, преимущества этой профессии.', 'partially-done'],
+      ],
+    },
+    {
+      actual: false,
+      date: '19.02.2022',
+      time: '30 мин',
+      list: [
+        ['Обсудить речь Speaker D - проблемы, трудности, преимущества этой профессии.', 'done'],
+        ['Разобрать речь Speaker E', 'partially-done'],
+      ],
+    },
+    {
+      actual: false,
+      date: '20.02.2022',
+      time: '30 мин',
+      list: [
+        ['Записать на английском речь Speaker E из Задания 176 на аудирование'],
+      ],
+    },
+  ],
+  // 21.02. - 27.02
+  [
+    {
+      actual: false,
+      date: '22.02.2022',
+      time: '30 мин',
+      list: [
+        ['Обсуждение личных и политических новостей', 'done'],
+      ],
+    },
+    {
+      // занятие перенесено
+    },
+    {
+      actual: true,
+      date: '26.02.2022',
+      time: '1 час',
+      list: [
+        ['Выполнение задания на подстановку пропусков по текущей политической ситуации'],
+      ],
+      links: [
         {
-          actual: false,
-          date: '08.02.2022',
-          time: '30 мин',
-          list: [
-            ['Записать на английском речь Speaker C из Задания 176 на аудирование', 'done'],
-            ['Разобрать речь Speaker D'],
-          ],
-        },
-        {},
-        {
-          actual: false,
-          date: '12.02.2022',
-          time: '60 мин',
-          list: [
-            ['Разобрать речь Speaker D', 'done'],
-          ],
-        },
-        {
-          actual: false,
-          date: '13.02.2022',
-          time: '30 мин',
-          list: [
-            ['Разговор на общие темы - про пищевые привычки в семье '],
-          ],
+          title: 'Some politics of 2022',
+          address: 'https://daria-blomma-tutoring.herokuapp.com/lesson-materials#politics_2022',
         },
       ],
-      [
-        {
-        },
-        {
-          actual: false,
-          date: '17.02.2022',
-          time: '30 мин',
-          list: [
-            ['Записать на английском речь Speaker D из Задания 176 на аудирование', 'done'],
-            ['Обсудить речь Speaker D - проблемы, трудности, преимущества этой профессии.', 'partially-done'],
-          ],
-        },
-        {
-          actual: true,
-          date: '19.02.2022',
-          time: '30 мин',
-          list: [
-            ['Обсудить речь Speaker D - проблемы, трудности, преимущества этой профессии.', 'partially-done'],
-            ['Разобрать речь Speaker E'],
-          ],
-        },
-        {
-          actual: false,
-          date: '20.02.2022',
-          time: '30 мин',
-          list: [
-            ['Записать на английском речь Speaker E из Задания 176 на аудирование'],
-            ['Обсудить речь Speaker E - проблемы, трудности, преимущества этой профессии.'],
-          ],
-        },
+    },
+    {
+      actual: false,
+      date: '27.02.2022',
+      time: '30 мин',
+      list: [
+        ['Разобрать речь Speaker E', 'partially-done'],
+        ['Записать на английском речь Speaker E из Задания 176 на аудирование'],
       ],
-      [
-        {
-          actual: false,
-          date: '22.02.2022',
-          time: '30 мин',
-          list: [
-            ['Разобрать речь Speaker F'],
-          ],
-        },
-        {
-          actual: false,
-          date: '24.02.2022',
-          time: '30 мин',
-          list: [
-            ['Записать на английском речь Speaker F из Задания 176 на аудирование'],
-            ['Обсудить речь Speaker F - проблемы, трудности, преимущества этой профессии.'],
-          ],
-        },
+    },
+  ],
+  // 28.02 - 06.03
+  [
+    {
+      actual: false,
+      date: '01.03.2022',
+      time: '30 мин',
+      list: [
+        ['Обсудить речь Speaker E - проблемы, трудности, преимущества этой профессии.'],
+        ['Разобрать речь Speaker F'],
+        ['Записать на английском речь Speaker F из Задания 176 на аудирование'],
+        ['Обсудить речь Speaker F - проблемы, трудности, преимущества этой профессии.'],
       ],
-    ];
-
-    return {
-      plansColorsList,
-      lessonPlans,
-    };
-  },
-};
+    },
+    {
+      actual: false,
+      date: '03.03.2022',
+      time: '30 мин',
+      list: [
+        ['Записать на английском речь Speaker F из Задания 176 на аудирование'],
+        ['Обсудить речь Speaker F - проблемы, трудности, преимущества этой профессии.'],
+      ],
+    },
+    {},
+    {},
+  ],
+];
 </script>
